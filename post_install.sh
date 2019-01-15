8a5919f240b4388c07de620c23d64a8df5100dd4
Adam Cooke <Adam.Cooke@CHHTexas.com>
2019-01-15 14:57:34 -0600

    changed source to tarball

--- post_install.sh
+++ post_install.sh
@@ -3,21 +3,19 @@
 # Create User
 pw useradd -n tautulli -c "Tautulli" -s /sbin/nologin -w no
 
-# Install from master
-cd /usr/local/share
-git clone https://github.com/Tautulli/Tautulli.git
-chown -R tautulli:tautulli Tautulli
+# Install
+fetch https://github.com/Tautulli/Tautulli/archive/master.tar.gz
+tar xfz master.tar.gz -C /usr/local/share/
+mv /usr/local/share/Tautulli-master /usr/local/share/tautulli
+
+chown -R tautulli:tautulli /usr/local/share/tautulli
 
 # Copy init script
-cp /usr/local/share/Tautulli/init-scripts/init.freenas /usr/local/etc/rc.d/tautulli
+cp /usr/local/share/tautulli/init-scripts/init.freenas /usr/local/etc/rc.d/tautulli
 
 # Configure/enable daemon
 sysrc -f /etc/rc.conf tautulli_user="tautulli"
 sysrc -f /etc/rc.conf tautulli_enable="YES"
-
-# Cleanup
-pkg delete -y git
-pw userdel git_daemon
 
 # Start service
 service tautulli start
