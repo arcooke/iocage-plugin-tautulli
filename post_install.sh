290ebe91fd02a9efe9b3115232587325dc683fc2
Adam Cooke <Adam.Cooke@CHHTexas.com>
2019-01-15 16:33:58 -0600

    revert to git clone

--- post_install.sh
+++ post_install.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Create User
-#pw useradd -n tautulli -c "Tautulli" -s /sbin/nologin -w no
-
 # Install
-fetch https://github.com/Tautulli/Tautulli/archive/master.tar.gz
-tar xfz master.tar.gz -C /usr/local/share/
-mv /usr/local/share/Tautulli-master /usr/local/share/Tautulli
-
-chmod -R 777 /usr/local/share/Tautulli
-#chown -R tautulli:tautulli /usr/local/share/Tautulli
+cd /usr/local/share
+git clone https://github.com/Tautulli/Tautulli.git
+chmod -R 777 Tautulli
 
 # Copy init script
 mkdir /usr/local/etc/rc.d
